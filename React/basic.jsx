040ac50fab04abd75ff0bc7f6676bab61e4d252c
Zahid Hasan <zahidhasan140338@gmail.com>
2023-12-23 21:35:18 +0600

    learned to use self closing tags in jsx

--- React/basic.jsx
+++ React/basic.jsx
@@ -1,9 +1,7 @@
 const JSX = (
     <div>
-      <h1>Hello World</h1>
-      <p>Lets render this to the DOM</p>
+      <h2>Welcome to React!</h2> <br />
+      <p>Be sure to close all tags!</p>
+      <hr />
     </div>
   );
- 
-  
-  ReactDOM.render(JSX, document.getElementById('challenge-node'))
