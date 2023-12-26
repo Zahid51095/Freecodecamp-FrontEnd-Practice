ced5994bf39c7c792fa9ad2556cd51679c009521
Zahid Hasan <zahidhasan140338@gmail.com>
2023-12-26 22:14:49 +0600

    learned to access the props using this.prop

---
+++ React/thisProps.jsx
@@ -0,0 +1,30 @@
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+  
+    }
+    render() {
+      return (
+          <div>
+              <Welcome name="Zahid"/>
+  
+          </div>
+      );
+    }
+  };
+  
+  class Welcome extends React.Component {
+    constructor(props) {
+      super(props);
+  
+    }
+    render() {
+      return (
+          <div>
+  
+            <p>Hello, <strong>{this.props.name}</strong>!</p>
+  
+          </div>
+      );
+    }
+  };
