7651e314f8586efe66ba5283fc5401235dfa4df1
Zahid Hasan <zahidhasan140338@gmail.com>
2023-12-31 16:37:40 +0600

    learned to Use the Lifecycle Method componentWillMount~

---
+++ React/willMount.jsx
@@ -0,0 +1,11 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    componentWillMount() {
+     console.log("hi this is me")
+    }
+    render() {
+      return <div />
+    }
+  };
