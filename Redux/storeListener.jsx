84f67ca3f3f3969c314122095dbd93772e625233
Zahid Hasan <zahidhasan140338@gmail.com>
2024-01-05 15:40:24 +0600

    learned to register a store listener

---
+++ Redux/storeListener.jsx
@@ -0,0 +1,25 @@
+const ADD = "ADD";
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+// Global count variable:
+let count = 0;
+
+const incrementCount = () => count++;
+store.subscribe(incrementCount);
+
+store.dispatch({ type: ADD });
+console.log(count);
+store.dispatch({ type: ADD });
+console.log(count);
+store.dispatch({ type: ADD });
+console.log(count);
